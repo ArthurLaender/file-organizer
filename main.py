1244693bdb3d7fac9fe9afc2e5205c3dba6bd1de
Arthur Laender <arthlaender@gmail.com>
2026-02-07 23:25:39 -0300

    add file categorization logic based on extension

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 # main.py
+from pathlib import Path
 from organizer.scanner import scan_folder
 from organizer.sorter import decide_folder
+from organizer.mover import move_file
 
 
 def main():
-    files = scan_folder(".")
+    base_path = Path(".")
+
+    files = scan_folder(base_path)
 
     for file in files:
-        folder = decide_folder(file)
-        print(f"{file.name} -> {folder}")
+        folder_name = decide_folder(file)
+        destination_folder = base_path / folder_name
+
+        new_path = move_file(file, destination_folder)
+        print(f"Moved {file.name} -> {new_path}")
 
 
 if __name__ == "__main__":
--- organizer/mover.py
+++ organizer/mover.py
@@ -1,1 +1,31 @@
 #mover.py
+
+from pathlib import Path
+#library for file operations
+import shutil
+
+def ensure_folder_exists(folder_path: Path):
+    folder_path.mkdir(parents=True, exist_ok=True)
+
+def get_safe_destination(destination: Path) -> Path:
+    if not destination.exists():
+        return destination
+
+    #guarantees no overwrite occurs by adding a counter suffix to the file name
+    counter = 1
+    while True:
+        new_destination = destination.with_stem(
+            f"{destination.stem}_{counter}"
+        )
+        if not new_destination.exists():
+            return new_destination
+        counter += 1
+
+def move_file(file_path: Path, destination_folder: Path) -> Path:
+    ensure_folder_exists(destination_folder)
+
+    destination = destination_folder / file_path.name
+    safe_destination = get_safe_destination(destination)
+
+    shutil.move(str(file_path), str(safe_destination))
+    return safe_destination
